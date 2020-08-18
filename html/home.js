9413236c386b54ec92b4fedf57c63a35a6c37002
Avery Swank <averyswank@aol.com>
2020-08-17 23:08:04 -0500

    Added loading animations. Added 'Data Display' filter

--- html/home.js
+++ html/home.js
@@ -8,6 +8,17 @@ const graph_height = 180
 const graph_piehole = 0.7
 const graph_legend = `none`
 
+// Google Charts graph colors for consistency
+const color_positive = `#ff0000`
+const color_negative = `#00fe00`
+const color_death = `#000000`
+const color_recovered = `#00ff00`
+const color_totalTests = `#33ccff`
+const color_pending = `#ffff99`
+const color_hospitalized = `#dedeff`
+const color_icu = `#ffffd4`
+const color_ventilator = `#fdd3d4`
+
 /**
  * @function fillTables
  * @description
@@ -15,13 +26,16 @@ const graph_legend = `none`
  */
 const fillTables = async () => {
     const us = await $.get(`/covidData/us`)
+    const usData = us[0]
     const states = await $.get(`/covidData/states`)
     const statesInfo = await $.get(`/data/states`)
 
     const statesTable = document.getElementById(stateTableId)
 
     // Create United States entity
-    createEntity(`us_entity`, `United States`, us[0])
+    createEntity(`us_entity`, `United States`, usData)
+    createTexts(`us_entity`, usData)
+    createGraphs(`us_entity`, usData)
 
     // Create each indiviaul state's entity
     for(var i = states.length-1; i >= 0; i--) {
@@ -40,13 +54,15 @@ const fillTables = async () => {
       const numRows = statesTable.getElementsByTagName(`tr`)
       const row = statesTable.insertRow(numRows)
 
-      // Create entity div to store
+      // Create entity div to store entity
       const stateEntity = document.createElement(`div`)
       stateEntity.className = `entity`
       stateEntity.id = stateAbbr + `_entity`
       row.appendChild(stateEntity)
 
-      createEntity(stateEntity.id, stateName, stateData)
+      createEntity(stateEntity.id, stateName)
+      createTexts(stateEntity.id, stateData)
+      createGraphs(stateEntity.id, stateData)
     }
 }
 
@@ -55,22 +71,59 @@ const fillTables = async () => {
  * @description
  *   Create an interactive piece of covid data that includes:
  *    - title of data
+ *    - a set of texts pertaining to cases, hospitalizations, deaths, etc.
  *    - a set of graphs pertaining to cases, hospitalizations, deaths, etc.
+ *    - a set of historical trends pertaining to cases, hospitalizations, deaths, etc.
+ *    - a standard loader
  */
-const createEntity = (entityId, title, data) => {
+const createEntity = async (entityId, title) => {
   const entity = document.getElementById(entityId)
 
+  // title
   const entityTitle = document.createElement(`h1`)
   entityTitle.className = `entity_title`
   entityTitle.innerHTML = title
   entity.appendChild(entityTitle)
 
+  // loading animation
+  const loader = document.createElement(`div`)
+  loader.className = `loader`
+  loader.id = `${entityId}_loader`
+  loader.hidden = false
+  entity.appendChild(loader)
+
+  // text table
+  const textTable = document.createElement(`table`)
+  textTable.className = `entity_text_table`
+  textTable.id = `${entityId}_text_table`
+  entity.appendChild(textTable)
+
+  // graph table
   const graphTable = document.createElement(`table`)
-  graphTable.className = `entity_table`
+  graphTable.className = `entity_graph_table`
   graphTable.id = `${entityId}_graph_table`
   entity.appendChild(graphTable)
 
-  createGraphs(entityId, data)
+  // historical table
+  const historicalTable = document.createElement(`table`)
+  historicalTable.className = `entity_historical_table`
+  historicalTable.id = `${entityId}_historical_table`
+  entity.appendChild(historicalTable)
+}
+
+function createTexts(entityId, data) {
+  const table = document.getElementById(`${entityId}_text_table`)
+  const row = table.insertRow(0)
+
+  var casesCell = row.insertCell(0)
+  var mortalityCell = row.insertCell(1)
+  var testingCell = row.insertCell(2)
+  var hospitalCell = row.insertCell(3)
+
+  casesCell.innerHTML = `Positive: ${data.positive} Negative: ${data.negative}`
+  mortalityCell.innerHTML = `Deaths: ${data.death} Recovered: ${data.recovered}`
+  testingCell.innerHTML = `Total Results: ${data.totalTestResults} Pending: ${data.pending}`
+  hospitalCell.innerHTML = `Hospitalized: ${data.hospitalizedCurrently} ICU: ${data.inIcuCurrently} On Ventilator: ${data.onVentilatorCurrently}`
 }
 
 function createGraphs(entityId, data) {
@@ -94,23 +147,6 @@ function createGraphs(entityId, data) {
   createHospitalGraph(hospitalCell.id, data)
 }
 
-/*function createRow(table, stateName = ``, stateData = {}) {
-  const numRows = table.getElementsByTagName(`tr`)
-  const row = table.insertRow(numRows.length)
-
-  var infoCell = row.insertCell(0)
-  var casesCell = row.insertCell(1)
-  var mortalityCell = row.insertCell(2)
-  var testingCell = row.insertCell(3)
-  var hospitalCell = row.insertCell(4)
-
-  infoCell.innerHTML = stateName
-  casesCell.innerHTML = `Positive: ${stateData.positive} Negative: ${stateData.negative}`
-  mortalityCell.innerHTML = `Deaths: ${stateData.death} Recovered: ${stateData.recovered}`
-  testingCell.innerHTML = `Total Results: ${stateData.totalTestResults} Pending: ${stateData.pending}`
-  hospitalCell.innerHTML = `Hospitalized: ${stateData.hospitalizedCurrently} ICU: ${stateData.inIcuCurrently} On Ventilator: ${stateData.onVentilatorCurrently}`
-}*/
-
 /**
  * @function createCasesGraph
  * @description
@@ -133,11 +169,11 @@ function createCasesGraph(elementId, data) {
       height: graph_height,
       pieHole: graph_piehole,
       legend: graph_legend,
-      colors: ['#ff0000', '#00fe00']  // Red, Green
+      colors: [color_positive, color_negative]
     }
       
     const chart = new google.visualization.PieChart(document.getElementById(elementId))
-    chart.draw(chartData, options);
+    chart.draw(chartData, options)
   }
 }
 
@@ -163,11 +199,11 @@ function createMortalityGraph(elementId, data) {
       height: graph_height,
       pieHole: graph_piehole,
       legend: graph_legend,
-      colors: ['#000000', '#00ff00']  // Black, Green
+      colors: [color_death, color_recovered]
     }
       
     const chart = new google.visualization.PieChart(document.getElementById(elementId))
-    chart.draw(chartData, options);
+    chart.draw(chartData, options)
   }
 }
 
@@ -193,11 +229,11 @@ function createTestingGraph(elementId, data) {
       height: graph_height,
       pieHole: graph_piehole,
       legend: graph_legend,
-      colors: ['#33ccff', '#ffff99']  // Sky Blue, Light Yellow
+      colors: [color_totalTests, color_pending]
     }
       
     const chart = new google.visualization.PieChart(document.getElementById(elementId))
-    chart.draw(chartData, options);
+    chart.draw(chartData, options)
   }
 }
 
@@ -224,10 +260,54 @@ function createHospitalGraph(elementId, data) {
       height: graph_height,
       pieHole: graph_piehole,
       legend: graph_legend,
-      colors: ['#dedeff', '#ffffd4', '#fdd3d4']  // Light Blue, Light Yellow, Light Red
+      colors: [color_hospitalized, color_icu, color_ventilator]
+    }
+
+    // Disable all loaders
+    const loaders = document.getElementsByClassName(`loader`)
+    for(const loader of loaders) {
+      loader.hidden = true
     }
       
     const chart = new google.visualization.PieChart(document.getElementById(elementId))
-    chart.draw(chartData, options);
+    chart.draw(chartData, options)
+  }
+}
+
+/**
+ * @function filterData
+ * @description
+ *   Hide/Show only the selected data display types: text, graph, historical
+ */
+function filterData(filter) {
+  const textTables = document.getElementsByClassName(`entity_text_table`)
+  const graphTables = document.getElementsByClassName(`entity_graph_table`)
+  const historicalTables = document.getElementsByClassName(`entity_historical_table`)
+
+  switch(filter) {
+    case 'text':
+      for(const textTable of textTables)
+        textTable.hidden = false
+      for(const graphTable of graphTables)
+        graphTable.hidden = true
+      for(const historicalTable of historicalTables)
+        historicalTable.hidden = true
+    break
+    case 'graph':
+      for(const textTable of textTables)
+        textTable.hidden = true
+      for(const graphTable of graphTables)
+        graphTable.hidden = false
+      for(const historicalTable of historicalTables)
+        historicalTable.hidden = true
+    break
+    case 'historical':
+      for(const textTable of textTables)
+        textTable.hidden = true
+      for(const graphTable of graphTables)
+        graphTable.hidden = true
+      for(const historicalTable of historicalTables)
+        historicalTable.hidden = false
+    break
   }
 }
